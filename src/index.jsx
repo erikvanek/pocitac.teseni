56855dc77a659fb4fc334f02414dfe014f07b538
vanekerik <erik.vanek@infarm.com>
2019-06-03 22:00:00 +0200

    feat: introduce tabs, add support for proper typography

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@
 import { Counter } from './counter.jsx'
 
 import ReactDOM from 'react-dom'
+import 'typeface-roboto'
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   entry:  path.join(__dirname, "src/index.jsx"),
   module: {
     rules: [
+      { test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader', options: { limit: 8192 } },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
